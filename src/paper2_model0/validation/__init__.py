9323d2d6bb5331bb9b2f640e7d44c38493144444
plinhnguyende-svg <plinhnguyen.de@gmail.com>
2026-09-18 12:05:43 +0200

    Add validation package

---
+++ src/paper2_model0/validation/__init__.py
@@ -0,0 +1,21 @@
+from .protocol import (
+    apply_parameter,
+    classify_ci_direction,
+    ofat_diagnostic,
+    phase_map_diagnostic,
+    replication_sufficiency_diagnostic,
+    seed_stability_diagnostic,
+    summarize_paired_effects,
+    warmup_convergence_diagnostic,
+)
+
+__all__ = [
+    "apply_parameter",
+    "classify_ci_direction",
+    "ofat_diagnostic",
+    "phase_map_diagnostic",
+    "replication_sufficiency_diagnostic",
+    "seed_stability_diagnostic",
+    "summarize_paired_effects",
+    "warmup_convergence_diagnostic",
+]
